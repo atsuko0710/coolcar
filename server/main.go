3885ac1076c22702172fadf85b0a6ff113a005cb
atsuko0710 <atsukodan10@gmail.com>
2021-07-28 15:17:40 +0800

    增加http监听

--- server/main.go
+++ server/main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
+	"net/http"
 
 	trippb "coolcar/server/proto/gen/go"
 	trip "coolcar/server/tripservice"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	go startGRPCGateway()
 	lis, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("failed to listen:%v", err)
@@ -20,3 +24,22 @@ func main() {
 	trippb.RegisterTripServiceServer(s, &trip.Service{})
 	log.Fatal(s.Serve(lis))
 }
+
+func startGRPCGateway() {
+	c := context.Background()
+	c, cannel := context.WithCancel(c)
+	defer cannel()
+
+	mux := runtime.NewServeMux()
+	// 与 8081 建立链接
+	err := trippb.RegisterTripServiceHandlerFromEndpoint(c, mux, "localhost:8081", []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Fatalf("cannot start grpc gateway:%v", err)
+	}
+
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatalf("cannot listen and server:%v", err)
+	}
+}
+ 
